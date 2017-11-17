0a9d27058f2ca020a46d5ec5a413a0aaa5786be6
jayrexacilo <acilojayrex@gmail.com>
2017-11-17 15:39:26 +0800

    Make GameCard component reusable with props

--- src/index.js
+++ src/index.js
@@ -4,4 +4,28 @@ import GameCard from "./components/GameCard";
 
 import "semantic-ui-css/semantic.min.css";
 
-render(<GameCard />, document.getElementById("root"));
+const games = [
+  {
+    name: "Quadropolis",
+    thumbnail: "https://cf.geekdo-images.com/images/pic2840020_md.jpg",
+    price: "32.99",
+    players: "2-4",
+    duration: "60"
+  },
+  {
+    name: "Five Tribes",
+    thumbnail: "https://cf.geekdo-images.com/images/pic2055255_md.jpg",
+    price: "51.00",
+    players: "2-4",
+    duration: "80"
+  },
+  {
+    name: "Roll for the Galaxy",
+    thumbnail: "https://cf.geekdo-images.com/images/pic1473629_md.jpg",
+    price: "22.99",
+    players: "2-5",
+    duration: "45"
+  }
+];
+
+render(<GameCard game={games[1]} />, document.getElementById("root"));
