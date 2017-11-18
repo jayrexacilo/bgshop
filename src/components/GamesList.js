5cebfd08ba6074dbd139640e0da463f1b5910fd5
jayrexacilo <acilojayrex@gmail.com>
2017-11-18 21:00:12 +0800

    Added Default props

--- src/components/GamesList.js
+++ src/components/GamesList.js
@@ -4,12 +4,26 @@ import GameCard from "./GameCard";
 
 const GamesList = ({ games }) => (
   <div className="ui four cards">
-    {games.map(game => <GameCard game={game} key={game._id} />)}
+    {games.length === 0 ? (
+      <div className="ui icon message">
+        <i className="icon info" />
+        <div className="content">
+          <div className="header">There are no game in your store!</div>
+          <p>You should add some, don't you think?</p>
+        </div>
+      </div>
+    ) : (
+      games.map(game => <GameCard game={game} key={game._id} />)
+    )}
   </div>
 );
 
 GamesList.propTypes = {
   games: PropTypes.arrayOf(PropTypes.object).isRequired
+};
+
+GamesList.defaultProps = {
+  games: []
 };
 
 export default GamesList;
